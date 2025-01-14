76323616121874beef878eb767b79b2482d3349a
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-01-14 22:52:59 +0200

    Update details.dart

--- lib/views/details.dart
+++ lib/views/details.dart
@@ -1,3 +1,4 @@
+import 'package:data_handel/service/cach_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:data_handel/models/user_model.dart';
 
@@ -8,56 +9,61 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //to save the viewed user to cache when the details page is opened
+    CacheHelper.saveViewedUser(user);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent,
         title: Text(
           '${user.firstName} ${user.lastName}',
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Container(
           height: 460,
+          width: 400,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
-              color: Color.fromARGB(255, 234, 227, 239),
-              border: Border.all(width: 0.7)),
+              color: const Color.fromARGB(255, 234, 227, 239),
+              border: Border.all(width: 0.1)),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 30,
                 ),
                 Image.network(user.image),
                 Text(
                   'ID: ${user.id}',
-                  style: TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18),
                 ),
                 Text(
                   'Name: ${user.firstName} ${user.lastName}',
-                  style: TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18),
                 ),
                 Text(
                   'Age: ${user.age}',
-                  style: TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18),
                 ),
                 Text(
                   'Gender: ${user.gender}',
-                  style: TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18),
                 ),
                 Text(
                   'Email: ${user.email}',
-                  style: TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18),
                 ),
                 Text(
                   'Phone: ${user.phone}',
-                  style: TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18),
                 ),
               ],
             ),
