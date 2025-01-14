398d64bbe7b888ccc4dc359a91038e008cc78126
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-01-14 22:57:34 +0200

    Update user_service.dart

--- lib/service/user_service.dart
+++ lib/service/user_service.dart
@@ -2,17 +2,22 @@ import 'package:dio/dio.dart';
 import 'package:data_handel/models/user_model.dart';
 
 class EmployeeService {
-  final Dio dio = Dio();
-  final String endpoint = 'https://dummyjson.com/users';
+  final Dio _dio = Dio(); //dio instance for HTTP requests
+  final String _endpoint = 'https://dummyjson.com/users'; 
 
   Future<UserResponse> fetchUsers() async {
     try {
-      final response = await dio.get(endpoint);
-      if (response.statusCode == 200) {
-        return UserResponse.fromJson(response.data);
+      final response = await _dio.get(_endpoint);     
+      if (response.statusCode == 200) {      //to check if the response is successful (status code 200)
+        return UserResponse.fromJson(response.data);    
       } else {
-        throw Exception('Failed to load users: Status Code ${response.statusCode}');
+        //throw an exception if the response is not successful
+        throw Exception(
+            'Failed to load users: Status Code ${response.statusCode}');
       }
+      //to handle errors and exceptions
+    } on DioException catch (e) {
+      throw Exception('Failed to load users: ${e.message}');
     } catch (e) {
       throw Exception('Failed to load users: $e');
     }
