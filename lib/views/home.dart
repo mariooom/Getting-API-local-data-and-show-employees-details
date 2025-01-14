5da22785ef3f5dd981bf8f258d2a934ee9151882
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-01-14 22:52:14 +0200

    Update home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:data_handel/models/user_model.dart';
 import 'package:data_handel/service/user_service.dart';
+
 import 'details.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,13 +19,14 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    fetchUsers();
+    fetchUsers(); //to fetch users when the page is initialized
   }
 
   Future<void> fetchUsers() async {
     try {
       final employeeService = EmployeeService();
       final userResponse = await employeeService.fetchUsers();
+
       setState(() {
         users = userResponse.users;
         loading = false;
@@ -42,21 +44,20 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.deepPurpleAccent,
         title: const Text(
-          'Employee List',
-          style: TextStyle(
+          'Employees List',
+          style: const TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
+        backgroundColor: Colors.deepPurpleAccent,
       ),
       body: loading
           ? const Center(child: CircularProgressIndicator())
           : errorMessage.isNotEmpty
-              ? Center(child: Text(errorMessage))
+              ? Center(child: Text(errorMessage)) //to show error message
               : ListView.builder(
-                  //display users' list
                   itemCount: users.length,
                   itemBuilder: (context, index) {
                     final user = users[index];
@@ -67,10 +68,11 @@ class _HomePageState extends State<HomePage> {
                       title: Text('${user.firstName} ${user.lastName}'),
                       subtitle: Text('Age: ${user.age}, Email: ${user.email}'),
                       onTap: () {
+                        //to navigate to details page
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => DetailsPage(user: user),        //route to each employee details page
+                            builder: (context) => DetailsPage(user: user),
                           ),
                         );
                       },
